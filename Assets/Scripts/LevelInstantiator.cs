a8f5baed65d6e850638238f2fb1294eabd263dc9
Pavlo Hornostaiev <goronostaev@gmail.com>
2023-10-28 16:00:00 +0300

    Refactorings for level instantiator

--- Assets/Scripts/LevelInstantiator.cs
+++ Assets/Scripts/LevelInstantiator.cs
@@ -15,16 +15,16 @@ public class LevelInstantiator : MonoBehaviour
 
     private GameObject currentObstacle;
 
-    public int currentLevel = 1;
-    public int addOn = 7;
-
-    private float i = 0;
+    public int currentLevel = 0;
+    private int obstaclesAmountMultiplier = 7;
 
     private void Start() => InstantiateLevel();
 
     private void InstantiateLevel()
     {
-        if (currentLevel > 9) addOn = 0;
+        currentLevel++;
+
+        if (currentLevel > 9) obstaclesAmountMultiplier = 0;
 
         SelectObstacleModel();
 
@@ -62,9 +62,9 @@ public class LevelInstantiator : MonoBehaviour
 
     private void InstantiateObstaclesForLevel()
     {
-        float random = Random.value;
+        float i;
 
-        for (i = 0; i > -currentLevel - addOn; i -= 0.5f)
+        for (i = 0; i > -currentLevel - obstaclesAmountMultiplier; i -= 0.5f)
         {
             if (currentLevel <= 20)
                 currentObstacle = Instantiate(currentLevelObstacles[Random.Range(0, 2)]);
@@ -78,28 +78,18 @@ public class LevelInstantiator : MonoBehaviour
             if (currentLevel > 100)
                 currentObstacle = Instantiate(currentLevelObstacles[Random.Range(3, 4)]);
 
-            currentObstacle.transform.position = new Vector3(0, i - 0.01f, 0);
-            currentObstacle.transform.eulerAngles = new Vector3(0, i * 8, 0);
-
             currentObstacle.transform.parent = obstaclesParent;
 
-            if (Mathf.Abs(i) >= currentLevel * 0.3f && Mathf.Abs(i) <= currentLevel * 0.6f)
-            {
-                currentObstacle.transform.eulerAngles = new Vector3(0, i * 8, 0);
-                currentObstacle.transform.eulerAngles += Vector3.up * 180f;
-            }
-            else if (Mathf.Abs(i) >= currentLevel * 0.8f)
-            {
-                currentObstacle.transform.eulerAngles = new Vector3(0, i * 8, 0);
+            float verticalPositionOffset = i - 0.01f;
+            currentObstacle.transform.position = new Vector3(0, verticalPositionOffset, 0);
 
-                if (random > 0.75f)
-                {
-                    currentObstacle.transform.eulerAngles += Vector3.up * 180f;
-                }
-            }
+            float rotationOffset = i * 8;
+            currentObstacle.transform.eulerAngles = new Vector3(0, rotationOffset, 0);
         }
 
         var floor = Instantiate(floorPrefab);
-        floor.transform.position = new Vector3(0, i - 0.01f, 0);
+
+        float floorPositionOffset = i - 0.01f;
+        floor.transform.position = new Vector3(0, floorPositionOffset, 0);
     }
 }
