905ac57835375deaa3e76da26a9de368f705b940
Pavlo Hornostaiev <goronostaev@gmail.com>
2023-10-28 16:07:55 +0300

    Refactorings for camera system

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -2,27 +2,29 @@ using UnityEngine;
 
 public class CameraSystem : MonoBehaviour
 {
-    [SerializeField] private Transform ball;
+    [SerializeField]
+    private Transform ball;
+
     private Transform floor;
 
     private Vector3 offset;
 
-    void Update()
-    {
-        FollowBall();
-    }
+    private void Update() => FollowBall();
 
     private void FollowBall()
     {
         if (floor == null)
             floor = GameObject.Find("Floor(Clone)").GetComponent<Transform>();
 
+        float offsetOnFloor = floor.position.y + 4f;
+
         if (transform.position.y > ball.position.y &&
-           transform.position.y > floor.position.y + 4f)
+            transform.position.y > offsetOnFloor)
         {
             offset = new Vector3(transform.position.x, ball.position.y, transform.position.z);
         }
 
-        transform.position = new Vector3(transform.position.x, offset.y, -5f);
+        float forwardOffset = -5f;
+        transform.position = new Vector3(transform.position.x, offset.y, forwardOffset);
     }
 }
