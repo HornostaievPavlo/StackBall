53cb68ad23cecf1fe34fbce0de3a92ef78abafed
Pavlo Hornostaiev <goronostaev@gmail.com>
2023-10-28 12:21:16 +0300

    Added basic input controls for ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rb;
+
+    private bool isFalling;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ProcessInput();
+    }
+
+    private void FixedUpdate()
+    {
+        ControlVelocity();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetMouseButtonDown(0)) isFalling = true;
+        if (Input.GetMouseButtonUp(0)) isFalling = false;
+    }
+
+    private void ControlVelocity()
+    {
+        Vector3 currentVelocity = rb.velocity;
+        float velocityMaxValue = 5f;
+
+        if (Input.GetMouseButton(0))
+        {
+            isFalling = true;
+
+            float verticalMovementFactor = -100f * Time.fixedDeltaTime * 7f;
+            currentVelocity = new Vector3(0, verticalMovementFactor, 0);
+        }
+        if (currentVelocity.y > velocityMaxValue)
+        {
+            currentVelocity = new Vector3(currentVelocity.x, velocityMaxValue, currentVelocity.z);
+        }
     }
 }
