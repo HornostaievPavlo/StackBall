01c69ba6a9d542272ff1ebd246dc8ea28206715d
Pavlo Hornostaiev <goronostaev@gmail.com>
2023-10-30 10:39:31 +0200

    Imported TMP essentials

---
+++ Assets/Scripts/UI/GameUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    
+}
